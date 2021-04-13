686efc649129e5c6756a07f674f4e834e4cbfdea
vazqjose <vazqjose@gmail.com>
2021-04-13 19:17:15 +0000

    renamed vars

--- global_var.h
+++ global_var.h
@@ -2,14 +2,14 @@
 #define GLOBAL_VAR_H
 
 #define max_args 13
-#define maxln_Com_Amb 105
+#define maxEnvCmd 105
 
 char *args[max_args]; /*reads command from user input*/
-char comando[maxln_Com_Amb]; /*store number of arguments from commands */
+char comando[maxEnvCmd]; /*store number of arguments from commands */
 
-char SHELL[maxln_Com_Amb];
-char PATH[maxln_Com_Amb];
-char HOME[maxln_Com_Amb];
-char PWD[maxln_Com_Amb];
+char SHELL[maxEnvCmd];
+char PATH[maxEnvCmd];
+char HOME[maxEnvCmd];
+char PWD[maxEnvCmd];
 
 #endif
