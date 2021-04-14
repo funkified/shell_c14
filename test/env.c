be387395554b1bf2318f21a342ee8d6337bdc4ba
Sergio Vera <sergio.vera@icloud.com>
2021-04-14 00:00:10 +0000

    holberton.h

---
+++ test/env.c
@@ -0,0 +1,63 @@
+#include "shell.h"
+#include "holberton.h"
+
+/**
+ * env_value - takes an evniroment string and gets the value in variable
+ * @str: string to be value
+ * @delim: chars where strtok split string
+ * Return: valie of th eenviromnet
+ */
+
+char *env_value(char *str, char *delim)
+
+{
+	str = strtok(str, delim);
+	str = strtok(NULL, delim);
+
+	return (str);
+}
+
+char *_getenv(char *name, char **env)
+{
+	int index, elements, len = 0;
+	char *env_string = NULL;
+
+	while(name[len])
+		len++;
+
+	env_string = malloc(sizeof(char) * len + 1);
+
+	if (env_string == NULL)
+	{
+		free(env_string);
+		return (NULL);
+	}
+	for (index = 0; env[index] != NULL; index++)
+	{
+		elements = 0;
+		while (elements < len)
+		{
+			env_string[elements] = env[index][elements];
+			elements++;
+		}
+		if (strcmp(name, env_string) == 0)
+		{
+			free(env_string);
+			return (env[index]);
+		}
+	}
+	free(env_string);
+	return (NULL);
+}
+
+/**
+ * _free = free malloc'd variable
+ * @d: struct to be free
+ * @Return: Nothing
+ */
+
+void _free(struct data *d)
+{
+	free(d->tokens);
+	free(d->buffer);
+}
---
+++ test/main.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+
+
+int main (int argc, char **argv)
+{
+	char *str = NULL, **args, *prompt = "$ ";
+	size_t len = 0;
+	ssize_t chr;
+	int count = 0;
+	(void)argc;
+
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, prompt, 2);
+		
+	signal(SIGINT, handler);
+	
+	while ((chr = getline(&str, &len, stdin)))
+	{
+		if (chr == EOF)
+		EOF(str);
+		++count;
+		args = strtok(str, prompt);
+		_fork();
+		len = 0;
+		str = NULL;
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, prompt, 2);
+	}
+	if (chr == -1)
+		return(EXIT_SUCCESS);
+		
+	return (EXIT_SUCCESS);
+}
